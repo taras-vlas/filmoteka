ef783bbf330e1f98a05997e52436c58ac5cd7d1d
Ivan Kuchvara <ivan.kuchvara@gmail.com>
2021-03-07 12:01:34 +0200

    ligoc start

---
+++ src/js/pagination.js
@@ -0,0 +1,16 @@
+const btn1Ref = document.querySelector('[data-index="1"]');
+const btn2Ref = document.querySelector('[data-index="2"]');
+const btn3Ref = document.querySelector('[data-index="3"]');
+const btn4Ref = document.querySelector('[data-index="4"]');
+const btn5Ref = document.querySelector('[data-index="5"]');
+const firstPageRef = document.querySelector('.first-button');
+const lastPageRef = document.querySelector('.last-button');
+const paginationRef = document.querySelector('.pagination-container');
+const rightArrowRef = document.querySelector('.arrow-right');
+const leftArrowRef = document.querySelector('.arrow-left');
+const prevDotsRef = document.querySelector('#previous');
+const afterDotsRef = document.querySelector('#after');
+
+console.log(afterDotsRef);
+
+// =========================
